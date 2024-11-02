27afdbc1a0b3b84f91bfdd4ca37f1f49bae68033
NexGen <nexgentechnologies2024@gmail.com>
2024-11-02 15:26:38 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NexGenWebMaintainer',
+  webDir: 'www'
+};
+
+export default config;
